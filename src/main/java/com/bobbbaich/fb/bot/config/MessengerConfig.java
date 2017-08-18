1b6572063b973e102c704a20c658bf7ce256ca40
bobbbaich <bpogoriliy@gmail.com>
2017-08-18 13:17:34 +0300

    check heroku CI intregration with github

---
+++ src/main/java/com/bobbbaich/fb/bot/config/MessengerConfig.java
@@ -0,0 +1,87 @@
+package com.bobbbaich.fb.bot.config;
+
+import com.github.messenger4j.MessengerPlatform;
+import com.github.messenger4j.receive.MessengerReceiveClient;
+import com.github.messenger4j.receive.handlers.TextMessageEventHandler;
+import com.github.messenger4j.send.MessengerSendClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("messenger4j.properties")
+public class MessengerConfig {
+    private static final Logger LOG = LoggerFactory.getLogger(MessengerConfig.class);
+
+    private TextMessageEventHandler textMessageEventHandler;
+
+    /**
+     * Initializes the {@code MessengerSendClient}.
+     *
+     * @param pageAccessToken the generated {@code Page Access Token}
+     */
+    @Bean
+    @Profile("remote")
+    public MessengerSendClient messengerSendClient(@Value("${messenger4j.pageAccessToken}") String pageAccessToken) {
+        LOG.debug("Initializing MessengerSendClient - pageAccessToken: {}", pageAccessToken);
+        return MessengerPlatform.newSendClientBuilder(pageAccessToken).build();
+    }
+
+    /**
+     * Initializes the {@code MessengerReceiveClient}.
+     *
+     * @param appSecret   the {@code Application Secret}
+     * @param verifyToken the {@code Verification Token} that has been provided by you during the setup of the {@code
+     *                    Webhook}
+     */
+
+    @Bean
+    @Profile("remote")
+    public MessengerReceiveClient messengerReceiveClient(@Value("${messenger4j.appSecret}") final String appSecret,
+                                                         @Value("${messenger4j.verifyToken}") final String verifyToken) {
+        LOG.debug("Initializing MessengerReceiveClient - appSecret: {} | verifyToken: {}", appSecret, verifyToken);
+        return MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
+                .onTextMessageEvent(textMessageEventHandler)
+                .build();
+    }
+
+    /**
+     * Initializes the {@code MessengerSendClient} for profile 'local'.
+     *
+     * @param pageAccessToken the generated {@code Page Access Token}
+     */
+    @Bean
+    @Profile("local")
+    public MessengerSendClient messengerSendClientDev(@Value("${local.messenger4j.pageAccessToken}") String pageAccessToken) {
+        LOG.debug("Initializing MessengerSendClient - pageAccessToken: {}", pageAccessToken);
+        return MessengerPlatform.newSendClientBuilder(pageAccessToken).build();
+    }
+
+    /**
+     * Initializes the {@code MessengerReceiveClient} for profile 'local'.
+     *
+     * @param appSecret   the {@code Application Secret}
+     * @param verifyToken the {@code Verification Token} that has been provided by you during the setup of the {@code
+     *                    Webhook}
+     */
+
+    @Bean
+    @Profile("local")
+    public MessengerReceiveClient messengerReceiveClientDev(@Value("${local.messenger4j.appSecret}") final String appSecret,
+                                                            @Value("${local.messenger4j.verifyToken}") final String verifyToken) {
+        LOG.debug("Initializing MessengerReceiveClient - appSecret: {} | verifyToken: {}", appSecret, verifyToken);
+        return MessengerPlatform.newReceiveClientBuilder(appSecret, verifyToken)
+                .onTextMessageEvent(textMessageEventHandler)
+                .build();
+    }
+
+    @Autowired
+    public void setTextMessageEventHandler(TextMessageEventHandler textMessageEventHandler) {
+        this.textMessageEventHandler = textMessageEventHandler;
+    }
+}
---
+++ src/main/java/com/bobbbaich/fb/bot/controller/MessengerPlatformCallbackHandler.java
@@ -0,0 +1,75 @@
+package com.bobbbaich.fb.bot.controller;
+
+import com.github.messenger4j.exceptions.MessengerVerificationException;
+import com.github.messenger4j.receive.MessengerReceiveClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import static com.github.messenger4j.MessengerPlatform.*;
+
+/**
+ * This is the main class for inbound and outbound communication with the Facebook Messenger Platform.
+ * The callback controller is responsible for the webhook verification and processing of the inbound messages and events.
+ * It showcases the features of the Messenger Platform.
+ */
+@RestController
+@RequestMapping("/callback")
+public class MessengerPlatformCallbackHandler {
+
+    private static final Logger LOG = LoggerFactory.getLogger(MessengerPlatformCallbackHandler.class);
+
+    private final MessengerReceiveClient receiveClient;
+
+    /**
+     * Constructs the {@code MessengerPlatformCallbackHandler}.
+     *
+     * @param receiveClient the initialized {@code MessengerReceiveClient}
+     */
+    @Autowired
+    public MessengerPlatformCallbackHandler(final MessengerReceiveClient receiveClient) {
+        this.receiveClient = receiveClient;
+    }
+
+    /**
+     * Webhook verification endpoint.
+     * <p>
+     * The passed verification token (as query parameter) must match the configured verification token.
+     * In case this is true, the passed challenge string must be returned by this endpoint.
+     */
+    @RequestMapping(method = RequestMethod.GET)
+    public ResponseEntity<String> verifyWebhook(@RequestParam(MODE_REQUEST_PARAM_NAME) final String mode,
+                                                @RequestParam(VERIFY_TOKEN_REQUEST_PARAM_NAME) final String verifyToken,
+                                                @RequestParam(CHALLENGE_REQUEST_PARAM_NAME) final String challenge) {
+
+        LOG.debug("Received Webhook verification request - mode: {} | verifyToken: {} | challenge: {}", mode,
+                verifyToken, challenge);
+        try {
+            return ResponseEntity.ok(this.receiveClient.verifyWebhook(mode, verifyToken, challenge));
+        } catch (MessengerVerificationException e) {
+            LOG.warn("Webhook verification failed: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body(e.getMessage());
+        }
+    }
+
+    /**
+     * Callback endpoint responsible for processing the inbound messages and events.
+     */
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> handleCallback(@RequestBody final String payload,
+                                               @RequestHeader(SIGNATURE_HEADER_NAME) final String signature) {
+
+        LOG.debug("Received Messenger Platform callback - payload: {} | signature: {}", payload, signature);
+        try {
+            this.receiveClient.processCallbackPayload(payload, signature);
+            LOG.debug("Processed callback payload successfully");
+            return ResponseEntity.status(HttpStatus.OK).build();
+        } catch (MessengerVerificationException e) {
+            LOG.warn("Processing of callback payload failed: {}", e.getMessage());
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build();
+        }
+    }
+}
