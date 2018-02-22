f818954b5630a34e61fff0fa49c067308ef5cfaa
bobbbaich <bpogoriliy1997>
2018-02-22 16:31:48 +0200

    add - kafka dependency, config

---
+++ src/main/java/com/bobbbaich/fb/bot/config/KafkaConfig.java
@@ -0,0 +1,12 @@
+package com.bobbbaich.fb.bot.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Slf4j
+@Configuration
+@EnableKafka
+public class KafkaConfig {
+
+}
