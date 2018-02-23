6d4398622639bfd95e68f8057b402c3fce48c69c
csipon <oxeygenoxeygen@gmail.com>
2018-02-23 11:45:48 +0200

    add shit docker to this shit project

--- src/main/java/com/bobbbaich/fb/bot/config/KafkaConfig.java
+++ src/main/java/com/bobbbaich/fb/bot/config/KafkaConfig.java
@@ -9,4 +9,30 @@ import org.springframework.kafka.annotation.EnableKafka;
 @EnableKafka
 public class KafkaConfig {
 
+//    @Autowired
+//    private KafkaProducerProperties kafkaProducerProperties;
+//
+//    @Bean
+//    public DefaultKafkaProducerFactory<String, String> producerFactory() {
+//        return new DefaultKafkaProducerFactory<>(producerConfigs());
+//    }
+//
+//    @Bean
+//    public Map<String, Object> producerConfigs() {
+//        Map<String, Object> props = new HashMap<>();
+//        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaProducerProperties.getBootstrap());
+//        return props;
+//    }
+//
+//    @Bean
+//    public KafkaTemplate<String, String> workUnitsKafkaTemplate() {
+//        KafkaTemplate<String, String> kafkaTemplate = new KafkaTemplate<>(producerFactory());
+//        kafkaTemplate.setDefaultTopic(kafkaProducerProperties.getTopic());
+//        return kafkaTemplate;
+//    }
+//
+//    @Bean
+//    public StringSerializer stringKeySerializer() {
+//        return new StringSerializer();
+//    }
 }
