6baa72dbb80d1b7f52177abf8d98a95d8aba32de
bobbbaich <bpogoriliy@gmail.com>
2018-04-09 21:52:29 +0300

    add - kafka hello world yopty rabotaet !!!!

--- src/main/java/com/bobbbaich/fb/bot/controller/TestController.java
+++ src/main/java/com/bobbbaich/fb/bot/controller/TestController.java
@@ -1,18 +1,29 @@
 package com.bobbbaich.fb.bot.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class TestController {
+    @Autowired
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+
     @GetMapping("/isAuth")
     public String isAuth() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         return String.valueOf(authentication != null);
+    }
+
+    @GetMapping("/kafka/send")
+    public String sendHello() {
+        kafkaTemplate.send("test", "Hello Kafka!");
+        return "Test message sent.";
     }
 }
--- src/main/java/com/bobbbaich/fb/bot/service/impl/FactProducerImpl.java
+++ src/main/java/com/bobbbaich/fb/bot/service/impl/FactProducerImpl.java
@@ -1,25 +1,17 @@
 package com.bobbbaich.fb.bot.service.impl;
 
-import com.bobbbaich.fb.bot.service.FactProducer;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 @Slf4j
 @RequiredArgsConstructor
 @Service
-public class FactProducerImpl implements FactProducer {
-    private final KafkaTemplate<String, String> kafkaTemplate;
+public class FactProducerImpl {
 
-    @Override
-    public void produce() {
-        kafkaTemplate.send("TextLinesTopic", "hello world!");
-    }
-
-    @KafkaListener(topics = "WordsWithCountsTopic")
+    @KafkaListener(topics = "test")
     public void listen(String message) {
-        System.out.println("Received Message: " + message);
+        System.out.println("Received Messasge in group foo: " + message);
     }
 }
