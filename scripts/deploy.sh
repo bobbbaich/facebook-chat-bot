8cfa3697445cb47925298484690b58c2f54e0173
bobbbaich <bpogoriliy1997>
2018-04-16 16:51:03 +0300

    add - ssh connect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,9 +1,12 @@
 #!/usr/bin/env bash
 
+apt-get install sshpass
+
 echo "Hello World!"
 
 ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 chmod 700 ~/.ssh
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$PROD_HOST_IP
 
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no" root@$PROD_HOST_IP
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no"
+sshpass -p $PROD_PASSWORD ssh -o StrictHostKeyChecking=no root@$PROD_HOST_IP
