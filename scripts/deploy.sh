c8ab227836f5446ee1c94d9c5f58601dee363eea
bobbbaich <bpogoriliy1997>
2018-04-16 16:27:57 +0300

    add - ssh connect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,9 +3,7 @@
 echo "Hello World!"
 
 ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-
 chmod 700 ~/.ssh
-
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$PROD_HOST_IP
 
-ssh -o "StrictHostKeyChecking no" root@$PROD_HOST_IP
+ssh -o StrictHostKeyChecking=no root@$PROD_HOST_IP
