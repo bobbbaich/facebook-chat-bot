390d0000f2328cdbafb34552409b0382f80b9328
bobbbaich <bpogoriliy1997>
2018-04-16 12:04:11 +0300

    add - travis set language add - fail test

--- src/test/java/com/bobbbaich/fb/bot/ApplicationTests.java
+++ src/test/java/com/bobbbaich/fb/bot/ApplicationTests.java
@@ -5,12 +5,14 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.junit.Assert.fail;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ApplicationTests {
 
     @Test
     public void contextLoads() {
+        fail();
     }
-
 }
