b2f671237aa994bc3ff1b9a3f557eb4c936ca350
bobbbaich <bpogoriliy1997>
2018-04-16 17:26:14 +0300

    add - ssh connect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ echo "Hello World!"
 
 ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 chmod 700 ~/.ssh
-ssh-copy-id -i -o StrictHostKeyChecking=no ~/.ssh/id_rsa.pub root@$PROD_HOST_IP
+ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no root@$PROD_HOST_IP
 
 ssh -oStrictHostKeyChecking=no root@$PROD_HOST_IP
