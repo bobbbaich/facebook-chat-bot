89c5e0a9428d4d6747d46153d029cd0117828b41
bobbbaich <bpogoriliy@gmail.com>
2018-04-16 23:00:33 +0300

    change - docker-compose version from 3 to 2

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,11 +1,18 @@
 #!/usr/bin/env bash
 
-echo "Hello World!"
-
-ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
-chmod 700 ~/.ssh
-ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no root@$PROD_HOST_IP
-cat ~/.ssh/id_rsa.pub | cat >> ~/.ssh/authorized_keys
-
-ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no root@$PROD_HOST_IP
+#echo "Hello World!"
+#
+#ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
+#chmod 700 ~/.ssh
+#ssh-copy-id -i ~/.ssh/id_rsa.pub -o StrictHostKeyChecking=no root@$PROD_HOST_IP
+#cat ~/.ssh/id_rsa.pub | cat >> ~/.ssh/authorized_keys
+#
+#ssh -o PasswordAuthentication=no -o StrictHostKeyChecking=no root@$PROD_HOST_IP
 #cat ~/.ssh/id_rsa.pub
+
+git pull
+
+cd ..
+cd docker
+
+docker-compose up
