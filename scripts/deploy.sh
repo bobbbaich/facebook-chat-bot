e304ae12742aaca9d8a49ec760a68aecc167c671
bobbbaich <bpogoriliy1997>
2018-04-16 16:37:52 +0300

    add - ssh connect

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,4 +6,4 @@ ssh-keygen -f ~/.ssh/id_rsa -t rsa -N ''
 chmod 700 ~/.ssh
 ssh-copy-id -i ~/.ssh/id_rsa.pub root@$PROD_HOST_IP
 
-ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking=no" root@$PROD_HOST_IP
+ssh -o "UserKnownHostsFile=/dev/null" -o "StrictHostKeyChecking no" root@$PROD_HOST_IP
