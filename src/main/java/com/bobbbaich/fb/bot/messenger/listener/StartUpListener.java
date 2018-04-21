bf501d114d74c592b59f3b5510e76192de650523
bobbbaich <bpogoriliy@gmail.com>
2018-04-21 19:14:00 +0300

    disable messenger signature

--- src/main/java/com/bobbbaich/fb/bot/messenger/listener/StartUpListener.java
+++ src/main/java/com/bobbbaich/fb/bot/messenger/listener/StartUpListener.java
@@ -1,11 +1,17 @@
 package com.bobbbaich.fb.bot.messenger.listener;
 
 import com.github.messenger4j.Messenger;
+import com.github.messenger4j.common.WebviewHeightRatio;
+import com.github.messenger4j.common.WebviewShareButtonState;
 import com.github.messenger4j.exception.MessengerApiException;
 import com.github.messenger4j.exception.MessengerIOException;
 import com.github.messenger4j.messengerprofile.MessengerSettings;
 import com.github.messenger4j.messengerprofile.getstarted.StartButton;
 import com.github.messenger4j.messengerprofile.greeting.Greeting;
+import com.github.messenger4j.messengerprofile.persistentmenu.PersistentMenu;
+import com.github.messenger4j.messengerprofile.persistentmenu.action.NestedCallToAction;
+import com.github.messenger4j.messengerprofile.persistentmenu.action.PostbackCallToAction;
+import com.github.messenger4j.messengerprofile.persistentmenu.action.UrlCallToAction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
@@ -14,7 +20,12 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
-import java.util.Optional;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Arrays;
+
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -30,14 +41,32 @@ public class StartUpListener implements ApplicationListener<ApplicationReadyEven
 
     @Override
     public void onApplicationEvent(ApplicationReadyEvent applicationReadyEvent) {
+
+        PostbackCallToAction callToActionAA = PostbackCallToAction.create("Pay Bill", "PAYBILL_PAYLOAD");
+        PostbackCallToAction callToActionAB = PostbackCallToAction.create("History", "HISTORY_PAYLOAD");
+        PostbackCallToAction callToActionAC = PostbackCallToAction.create("Contact Info", "CONTACT_INFO_PAYLOAD");
+
+        NestedCallToAction callToActionA = NestedCallToAction.create("My Account",
+                Arrays.asList(callToActionAA, callToActionAB, callToActionAC));
+
+        UrlCallToAction callToActionB = null;
+        try {
+            callToActionB = UrlCallToAction.create("Google",
+                    new URL("https://www.google.com.ua"), of(WebviewHeightRatio.FULL), empty(), empty(),
+                    of(WebviewShareButtonState.HIDE));
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+
         MessengerSettings messengerSettings = MessengerSettings
-                .create(Optional.of(StartButton.create(startedButton)),
-                        Optional.of(Greeting.create(greeting)),
-                        Optional.empty(),
-                        Optional.empty(),
-                        Optional.empty(),
-                        Optional.empty(),
-                        Optional.empty());
+                .create(of(StartButton.create(startedButton)),
+                        of(Greeting.create(greeting)),
+                        of(PersistentMenu.create(true, of(Arrays.asList(callToActionA, callToActionB)))),
+                        empty(),
+                        empty(),
+                        empty(),
+                        empty());
         try {
             messenger.updateSettings(messengerSettings);
         } catch (MessengerApiException | MessengerIOException e) {
