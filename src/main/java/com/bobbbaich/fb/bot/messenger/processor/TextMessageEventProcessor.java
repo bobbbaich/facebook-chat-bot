6d3188ee05ba812d1cc9bb70ca4c4d9abc686497
Csipon <oxeygenoxeygen@gmail.com>
2018-05-01 21:11:04 +0300

    add - bugs

--- src/main/java/com/bobbbaich/fb/bot/messenger/processor/TextMessageEventProcessor.java
+++ src/main/java/com/bobbbaich/fb/bot/messenger/processor/TextMessageEventProcessor.java
@@ -1,19 +1,24 @@
 package com.bobbbaich.fb.bot.messenger.processor;
 
+import com.bobbbaich.fb.bot.messenger.service.AnalyseService;
 import com.github.messenger4j.webhook.Event;
 import com.github.messenger4j.webhook.event.TextMessageEvent;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.util.Assert;
 
 @Slf4j
+@RequiredArgsConstructor
 @Component
 public class TextMessageEventProcessor implements EventProcessor<TextMessageEvent> {
+    private final AnalyseService analyseService;
+
     @Override
     public void doProcessing(TextMessageEvent event) {
         log.debug("TextMessageEvent has been processing...");
+        analyseService.startAnalyse(event.recipientId(), event.text());
 
-        
     }
 
     @Override
