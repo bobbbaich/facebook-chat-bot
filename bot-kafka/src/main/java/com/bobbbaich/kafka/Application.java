ace06161e1dbfd352e948b152e4f730c7c9c738a
csipon <oxeygenoxeygen@gmail.com>
2018-05-08 15:37:12 +0300

    change

--- bot-kafka/src/main/java/com/bobbbaich/kafka/Application.java
+++ bot-kafka/src/main/java/com/bobbbaich/kafka/Application.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-@EnableEurekaClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
