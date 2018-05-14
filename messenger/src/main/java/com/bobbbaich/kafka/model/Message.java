390d20e0c9a4d6875941873bff7f896373936144
csipon <oxeygenoxeygen@gmail.com>
2018-05-14 15:20:20 +0300

    change

--- messenger/src/main/java/com/bobbbaich/kafka/model/Message.java
+++ messenger/src/main/java/com/bobbbaich/kafka/model/Message.java
@@ -1,10 +1,12 @@
 package com.bobbbaich.kafka.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
 @NoArgsConstructor
 @Builder
 public class Message {
